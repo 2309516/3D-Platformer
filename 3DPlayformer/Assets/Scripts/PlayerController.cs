3401f35a0765ba3507392c73df2c18b566821959
Bradley Curtis <2309516@students.ucreative.ac.uk>
2024-11-05 18:08:52 +0000

    Added more onto the scene and started making a main menu
    Next steps is to flesh out the environment a bit more. Finish by Friday.

--- 3DPlayformer/Assets/Scripts/PlayerController.cs
+++ 3DPlayformer/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    public float playerSpeed = 15f, jumpForce = 30f;
+    public float playerSpeed = 15f, jumpForce = 30f, groundDrag = 5f;
+    public float airControlFactor = 0.5f;
 
     private Rigidbody _rb;
     private bool _isGrounded;
@@ -19,21 +20,39 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
         Vector3 cameraForward = Vector3.Scale(_mainCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
         Vector3 movement = (cameraForward * moveVertical + _mainCamera.transform.right * moveHorizontal).normalized;
-
-        _rb.AddForce(movement * playerSpeed);
+        if (_isGrounded)
+        {
+            _rb.AddForce(movement * playerSpeed, ForceMode.Force);
+        }
+        else
+        {
+            _rb.AddForce(movement * playerSpeed * airControlFactor, ForceMode.Force);
+        }
 
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.5f)) _isGrounded = true;
-        else _isGrounded = false;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.5f))
+            _isGrounded = true;
+        else
+            _isGrounded = false;
 
+        // Jump logic
         if (Input.GetKey("space") && _isGrounded)
         {
-            Vector3 jump = new Vector3(0.0f, (jumpForce * 10), 0.0f);
-            _rb.AddForce(jump);
+            Vector3 jump = new Vector3(0.0f, jumpForce, 0.0f);
+            _rb.AddForce(jump, ForceMode.Impulse);
             _isGrounded = false;
+        }
+
+        if (_isGrounded)
+        {
+            _rb.drag = groundDrag;
+        }
+        else
+        {
+            _rb.drag = 0f;
         }
     }
 }
+
